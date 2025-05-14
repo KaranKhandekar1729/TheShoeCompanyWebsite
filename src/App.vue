81d0eb8e06644acd974c55f517884d1a49d77abb
Karan <karankhandekar028@gmail.com>
2025-05-15 01:44:18 +0530

    done with html and template structure

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
+import HowItWorks from './components/HowItWorks.vue'
+import AboutUs from './components/AboutUs.vue'
+import Testimonials from './components/Testimonials.vue'
+import Contact from './components/Contact.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <Header />
+</template>
