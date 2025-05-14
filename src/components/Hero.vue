ae01ad7001cc3c06e8548bff18f3b12c350a6d28
Karan <karankhandekar028@gmail.com>
2025-05-15 01:57:45 +0530

    Reapply "Reapply "done with html and template structure""
    This reverts commit 6e711d3e12c829ca809353295d9a32f0afaf39ec.

---
+++ src/components/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="bg-[url('./assets/images/hero-background.png')]">
+        <div>
+            <h1>
+                THE <br/>
+                SHOE <br/>
+                COMPANY <br/>
+            </h1>
+            <div>
+                <img src="./assets/images/hero-image.png" alt="hero image">
+            </div>
+        </div>
+    </section>
+</template>
